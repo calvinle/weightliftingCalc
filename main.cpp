254048c032ffbf91c028e48c705020fdc8ae337c
calvinle <calvin.md.le@gmail.com>
2018-05-03 11:48:35 -0600

    User variables

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int const barbell = 20;
-int const collar = 2.5;
+int const collars = 5.0;
 
 double const red = 25.0;
 double const blue = 20.0;
@@ -15,3 +15,7 @@ double const microBlack = 2.5;
 double const microWhite0 = 1.25;
 double const microWhite1 = 0.5;
 double const microWhite2 = 0.25;
+
+double totalWeight;
+double loadingWeight = totalWeight - barbell - collars;
+

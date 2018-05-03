7fa1aba48356cce35f247a70ea09c7f406d4ad57
calvinle <calvin.md.le@gmail.com>
2018-05-03 11:09:50 -0600

    Weights of discs, collars, bar

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "split.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int const barbell = 20;
+int const collar = 2.5;
+
+double const red = 25.0;
+double const blue = 20.0;
+double const yellow = 15.0;
+double const green = 10.0;
+double const white = 5.0;
+double const microBlack = 2.5;
+double const microWhite0 = 1.25;
+double const microWhite1 = 0.5;
+double const microWhite2 = 0.25;

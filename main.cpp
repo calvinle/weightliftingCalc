6f7e3a483603121c05552a75bc3ba2ef5d18e0ca
calvinle <calvin.md.le@gmail.com>
2018-05-16 16:48:37 -0600

    Current: Error input results in ending program. Icebox: Implement continuous input after error input.

--- main.cpp
+++ main.cpp
@@ -11,19 +11,16 @@ int main (int argc, char *argv[]) {
     std::cout << "ENTER WEIGHT: ";
     std::cin >> totalWeight;
     if (std::cin.fail()){
-      std::cout << "Wut" << std::endl;
-      std::cin.clear();
-      continue;
+      std::cout << "Error Input" << std::endl;
+      return -1;
     }
 
     if (totalWeight <= 25){ std::cerr << "No plates" << "\n" << std::endl; continue; }
     if (fmod(totalWeight, 2.5) != 0){
-      std::cerr << "Weight must be of 2.5kg increments" << "\n" << std::endl;
-      continue;
-    }
+      std::cerr << "Weight must be divisible by 2.5" << "\n" << std::endl; continue; }
 
-    //Subtract barbell and collars
-    discCalc(totalWeight - 25);
+    std::cout << "LOADING WEIGHT: " << totalWeight - 25 << std::endl;
+    discCalc(totalWeight - 25); //Subtract barbell and collars
   }
   return 0;
 }
